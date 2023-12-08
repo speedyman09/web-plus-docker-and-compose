3458b5a469575f4bad9fd09ff79f323b9ffba06c
Stephen <stephen.jr@selyuk.com>
2023-12-08 19:06:16 +0200

    Enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,14 +5,14 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // TODO: ENABLE CORS
-  // app.use(
-  //   cors({
-  //     origin: [
-  //       'https://stepanseliuk.nomoredomainsmonster.ru'
-  //     ],
-  //   }),
-  // );
+  app.use(
+    cors({
+      origin: [
+        'http://stepanseliuk.nomoredomainsmonster.ru',
+        'https://stepanseliuk.nomoredomainsmonster.ru',
+      ],
+    }),
+  );
   await app.listen(4000);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
