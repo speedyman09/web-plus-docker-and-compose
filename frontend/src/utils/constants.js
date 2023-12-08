d9b6366c78eff8f89d13cccd75996ba6528a576e
Stephen <stephen.jr@selyuk.com>
2023-12-08 19:10:19 +0200

    Somehow forgot to add https??

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "api.stepanseliuk.nomoredomainsmonster.ru";
+export const URL = "https://api.stepanseliuk.nomoredomainsmonster.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
